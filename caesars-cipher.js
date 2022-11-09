b7a0a413841f2d41ab4655c786aef42be1d92999
nivwer <nivwerr@gmail.com>
2022-11-08 23:07:59 -0300

    update caesars-cipher.js

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -1,5 +1,35 @@
+
+
+
+
 function rot13(str) {
-    return str;
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let answer = "";
+
+  for (let i = 0; i < str.length; i++){
+
+    let index = alphabet.indexOf(str[i])
+    
+    if (index >= 13){
+      answer += alphabet[index - 13]
+    } else if (index < 13 && index > -1){ 
+    answer += alphabet[index + 13]
+    } else {
+      answer += str[i]
+    }
   }
-  
-  rot13("SERR PBQR PNZC");
+  return answer;
+}
+
+
+function cipher(){
+  let text = document.getElementById("text").value;
+  hola = rot13(text);
+  console.log(hola);
+}
+
+
+
+
+
+
